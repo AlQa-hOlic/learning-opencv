59884f079773cfbd6f818b3221994ba294ba6e85
Rohit Krishnan <rohit.k.kesavan@gmail.com>
2019-12-14 19:45:17 +0800

    added matrix logic

--- week1/main.cpp
+++ week1/main.cpp
@@ -1,8 +1,45 @@
-#include <stdio.h>
+#include <iostream>
+
+
+int** identityMatrix(int rows, int cols) {
+    int** arr = new int*[rows];
+
+    for(int i = 0; i < rows; i++) {
+        arr[i] = new int[cols];
+
+        for(int j = 0; j < cols; j++) {
+            arr[i][j] = i == j ? 1 : 0;
+        }
+    }
+
+    return arr;
+}
+int** zeroMatrix(int rows, int cols) {
+    int** arr = new int*[rows];
+
+    for(int i = 0; i < rows; i++) {
+        arr[i] = new int[cols];
+
+        for(int j = 0; j < cols; j++) {
+            arr[i][j] = 0;
+        }
+    }
+    
+    return arr;
+}
+
+void printMatrix(int** arr, int rows, int cols) {
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            std::cout << arr[i][j] << ((j == cols - 1) ? "" : "\t");
+        }
+        std::cout << std::endl;
+    }
+}
 
 int main(int argc,char** argv) {
-    char* input = new char[16];
-    scanf("%s", input);
-    printf("You entered: %s\n",input);
+    std::cout << "Hello, World!" << std::endl;
+
+    printMatrix(zeroMatrix(2,2),2,2);
     return 0;
 }
