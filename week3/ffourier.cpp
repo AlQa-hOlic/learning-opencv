c2b7a7018b0ff951d11e9415d344db0108292aa4
Rohit Krishnan <rohit.k.kesavan@gmail.com>
2019-12-21 17:02:24 +0800

    🤣 the real fourier transform

--- week3/ffourier.cpp
+++ week3/ffourier.cpp
@@ -10,25 +10,53 @@ Mat fourier, ifourier;
 
 int main(int argc, char **argv)
 {
-    Mat src = imread(samples::findFile("./../test.jpg"), IMREAD_GRAYSCALE);
+    Mat I = imread( samples::findFile( "./../lena.tiff" ), IMREAD_GRAYSCALE);
+    Mat padded;
+    int m = getOptimalDFTSize( I.rows );
+    int n = getOptimalDFTSize( I.cols );
+    copyMakeBorder(I, padded, 0, m - I.rows, 0, n - I.cols, BORDER_CONSTANT, Scalar::all(0));
 
-    int m = getOptimalDFTSize( src.rows );
-    int n = getOptimalDFTSize( src.cols );
-    copyMakeBorder(src, src, 0, m - src.rows, 0, n - src.cols, BORDER_CONSTANT, Scalar::all(0));
+    Mat planes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)}; // [real, complex]
+    Mat complexI;
+    merge(planes, 2, complexI); // [real, complex] ==> Mat
 
-    src.convertTo(src,CV_64FC1, 1.0f/255.0f);
+    dft(complexI, complexI);
 
-    dft(src, fourier);
+    split(complexI, planes); // Mat ==> [real, complex]
+    magnitude(planes[0], planes[1], planes[0]); 
 
-    imshow("Original", src);
+    // switch to logarithmic scale [log(1+M)]
+    Mat magI = planes[0];
+    magI += Scalar::all(1);
+    log(magI, magI);
+
+    // //SWAP QUADRANTS
+    magI = magI(Rect(0, 0, magI.cols & -2, magI.rows & -2)); // now magI is an (even x even) Mat
     
-    imshow("Fourier Transform", fourier);
-    moveWindow("Fourier Transform", 50, 50);
+    int cx = magI.cols/2;
+    int cy = magI.rows/2;
+    
+    Mat q0(magI, Rect(0, 0, cx, cy));
+    Mat q1(magI, Rect(cx, 0, cx, cy));
+    Mat q2(magI, Rect(0, cy, cx, cy));
+    Mat q3(magI, Rect(cx, cy, cx, cy));
+    
+    Mat tmp;
 
-    idft(fourier, ifourier, CV_HAL_DFT_SCALE);
+    //first Q to fourth Q
+    q0.copyTo(tmp);
+    q3.copyTo(q0);
+    tmp.copyTo(q3);
 
-    imshow("Inverse Fourier Transform", ifourier);
-    moveWindow("Inverse Fourier Transform", 100, 100);
+    //second Q to third Q
+    q1.copyTo(tmp);
+    q2.copyTo(q1);
+    tmp.copyTo(q2);
+
+    normalize(magI, magI, 0, 1, NORM_MINMAX);
+
+    imshow("input", I   );
+    imshow("fourier", magI);
 
     while (1)
     {
