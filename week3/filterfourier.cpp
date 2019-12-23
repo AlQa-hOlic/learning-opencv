e56bede917579ea9b0165b6025e3b0c3e37edee5
Rohit Krishnan <rohit.k.kesavan@gmail.com>
2019-12-24 01:39:28 +0800

    refactor to avoid nan in some cases

--- week3/filterfourier.cpp
+++ week3/filterfourier.cpp
@@ -19,10 +19,10 @@ int main(int argc, char **argv)
     namedWindow("threshold");
     namedWindow("dst");
 
-    createTrackbar("Mask Threshold", "src", &threshold_value, 255, on_trackbar_change, 0);
-    
     src = imread(samples::findFile("./../test.jpg"), IMREAD_GRAYSCALE);
 
+
+    createTrackbar("Mask Threshold", "src", &threshold_value, 255, on_trackbar_change, 0);
     on_trackbar_change(0,0);
     
     while (1)
@@ -36,37 +36,74 @@ int main(int argc, char **argv)
     return 0;
 }
 
+void visualizeFourier(Mat& src, Mat& dst) {
+    // normalize values and switch to  logarithmic scale => log(1 + M);
+    dst = src.clone();
+    dst += Scalar::all(1.0f);
+    log(dst, dst);
+
+    normalize(dst, dst, 0, 1, NORM_MINMAX);
+    
+}
+
+void flipQuads(Mat& src, Mat& dst) {
+    dst = src.clone();
+    // switch quadrants for better visualization
+    dst = dst(Rect(0, 0, dst.cols & -2, dst.rows & -2)); // now magI is an (even x even) Mat
+    
+    int cx = dst.cols/2;
+    int cy = dst.rows/2;
+    
+    Mat q0(dst, Rect(0, 0, cx, cy));
+    Mat q1(dst, Rect(cx, 0, cx, cy));
+    Mat q2(dst, Rect(0, cy, cx, cy));
+    Mat q3(dst, Rect(cx, cy, cx, cy));
+    
+    Mat tmp;
+
+    //first Q to fourth Q
+    q0.copyTo(tmp);
+    q3.copyTo(q0);
+    tmp.copyTo(q3);
+
+    //second Q to third Q
+    q1.copyTo(tmp);
+    q2.copyTo(q1);
+    tmp.copyTo(q2);
+}
 
 void on_trackbar_change(int, void*) {
 
-    Mat srcFloat;
-    src.convertTo(srcFloat, CV_32F);
-
     Mat fourierTransform,fourierTransformNormalized;
-    dft(srcFloat, fourierTransform, DFT_REAL_OUTPUT);
+    Mat fourierTransformPlanes[] = {Mat_<float>(src), Mat::zeros(src.size(), CV_32F)};
+    Mat complexSrc;
+    merge(fourierTransformPlanes, 2, complexSrc);
+    dft(complexSrc, complexSrc);
+    split(complexSrc, fourierTransformPlanes);
+    magnitude(fourierTransformPlanes[0], fourierTransformPlanes[1], fourierTransform);
 
-    fourierTransform.copyTo(fourierTransformNormalized);
-    fourierTransformNormalized += Scalar::all(1);
-    log(fourierTransformNormalized, fourierTransformNormalized);
-    normalize(fourierTransformNormalized, fourierTransformNormalized, 0.0f, 1.0f, NORM_MINMAX);    
+    visualizeFourier(fourierTransform, fourierTransformNormalized);
 
-    cout << fourierTransformNormalized(Rect(150,150,10,10)) << endl;
     
     Mat fourierMask;
-    threshold(fourierTransformNormalized, fourierMask, (float)threshold_value/255.0f , 1.0f, THRESH_BINARY_INV);
-    
+    threshold(fourierTransformNormalized, fourierMask, (float)threshold_value/255.0 , 1.0, THRESH_BINARY_INV);
+    // fourierMask = fourierTransformNormalized;
     Mat fourierMaskBit;
     fourierMask.convertTo(fourierMaskBit, CV_8UC1);
 
     Mat fourierMasked;
-    fourierTransform.copyTo(fourierMasked, fourierMaskBit);
+    complexSrc.copyTo(fourierMasked, fourierMaskBit);
     
     Mat inverseTransform;
     dft(fourierMasked, inverseTransform, DFT_SCALE|DFT_INVERSE|DFT_REAL_OUTPUT);
+    cout << inverseTransform(Rect(150,150,10,10)) << endl;
 
     inverseTransform.convertTo(dst, CV_8U);
 
     imshow("src",src);
+
+    flipQuads(fourierMask,fourierMask);
     imshow("threshold",fourierMask);
+    
     imshow("dst", dst);
 }
